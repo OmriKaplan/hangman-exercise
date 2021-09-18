821fedf1f2219c702e075f45ed5149722d630c7a
Omri Kaplan <okaplan@lightricks.com>
2021-09-18 11:59:12 +0300

    add connection

--- test_game_method.py
+++ test_game_method.py
@@ -3,9 +3,12 @@ from unittest.mock import Mock
 
 def test_init_game():
     https_connection = HTTPConnection("a_path")
-    https_connection.call_init = \
-        Mock(return_value={"hangman": "", "token": "a_token"})
+    https_connection.call_init = Mock(
+        return_value={"hangman": "", "token": "a_token"}
+    )
     method = HTTPSMethod(https_connection)
+
     result = method.initialize_method()
+
     assert result == ""
 
